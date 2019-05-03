d086366cc7b52dc18777a4bbb28eadf1c8d1088c
Nitish Makhija <nitishmakhija@gmail.com>
2019-05-04 00:54:21 +0530

    * swift 4.2 compatibility

--- Sources/PerfectMiddleware/RouteContext.swift
+++ Sources/PerfectMiddleware/RouteContext.swift
@@ -5,7 +5,7 @@ import PerfectHTTP
  * @author Benoit BRIATTE http://www.digipolitan.com
  * @copyright 2017 Digipolitan. All rights reserved.
  */
-public protocol RouteContext: class {
+public protocol RouteContext: AnyObject {
     /** Retrieves the request */
     var request: HTTPRequest { get }
     /** Retrieves the response */
