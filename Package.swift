38fa827387c575917941a7b0e93242b928456054
Benoit Briatte <briatte.benoit@digipolitan.com>
2019-05-08 15:27:29 +0200

    update swift 5.0

--- Package.swift
+++ Package.swift
@@ -1,12 +1,23 @@
-// swift-tools-version:3.1
+// swift-tools-version:5.0
 
 import PackageDescription
 
 let package = Package(
     name: "PerfectMiddleware",
+    products: [
+        .library(
+            name: "PerfectMiddleware",
+            targets: ["PerfectMiddleware"]),
+    ],
     dependencies: [
-                .Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git",
-                        majorVersion: 2,
-                        minor: 2)
-        ]
+        .package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.0")
+    ],
+    targets: [
+        .target(
+            name: "PerfectMiddleware",
+            dependencies: ["PerfectHTTPServer"]),
+        .testTarget(
+            name: "PerfectMiddlewareTests",
+            dependencies: ["PerfectHTTPServer"])
+    ]
 )
